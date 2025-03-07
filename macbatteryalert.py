c35dfc71970b6a084a011f7500557b1b51b8dfba
Ibrahim Mudassar <22484328+ibrahimmudassar@users.noreply.github.com>
2025-03-06 21:36:24 -0500

    add even more severe alert
    if my mac is <=30% then alert me no matter how far I am from home because I need to charge it if I want a full day's worth of work tomorrow

--- macbatteryalert.py
+++ macbatteryalert.py
@@ -23,8 +23,10 @@ def calculate_distance(lat1, lon1, lat2, lon2):
     # Haversine formula
     dlat = lat2 - lat1
     dlon = lon2 - lon1
-    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * \
-        math.cos(lat2) * math.sin(dlon / 2) ** 2
+    a = (
+        math.sin(dlat / 2) ** 2
+        + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+    )
     c = 2 * math.asin(math.sqrt(a))
     r = 6371  # Radius of earth in meters
     return c * r
@@ -33,18 +35,19 @@ def calculate_distance(lat1, lon1, lat2, lon2):
 r = requests.get("http://ip-api.com/json/").json()
 
 gc = pygsheets.authorize(
-    service_file="/client_secret.json")
+    service_file="/Users/ibrahimmudassar/downloads/macbattery/client_secret.json"
+)
 raw_battery_data = os.popen("pmset -g batt").read()
-formatted_battery_data = re.search(
-    r"\s(\d+)\%\;.(.+)\;.(.+).present", raw_battery_data)
+formatted_battery_data = re.search(r"\s(\d+)\%\;.(.+)\;.(.+).present", raw_battery_data)
 
 # Open GoogleSheet
-sh = gc.open_by_key(env('SHEET_KEY'))
+sh = gc.open_by_key(env("SHEET_KEY"))
 wks = sh.sheet1
 
 # retrieve all rows of the worksheet
 cells = wks.get_all_values(
-    include_tailing_empty_rows=False, include_tailing_empty=False, returnas='matrix')
+    include_tailing_empty_rows=False, include_tailing_empty=False, returnas="matrix"
+)
 
 # extract all non empty rows
 nonEmptyRows = []
@@ -52,24 +55,36 @@ for i in cells:
     if i != []:
         nonEmptyRows.append(i)
 percentage, battery_status, battery_estimate = formatted_battery_data.groups()
-row = [percentage, battery_status, battery_estimate, r['lat'], r['lon'],
-       datetime.now().isoformat()]
+row = [
+    percentage,
+    battery_status,
+    battery_estimate,
+    r["lat"],
+    r["lon"],
+    datetime.now().isoformat(),
+]
 
 # here wks is the worksheet object
 wks.insert_rows(len(nonEmptyRows), values=row, inherit=True)
 
 distance_from_house = calculate_distance(
-    float(env("LAT")), float(env("LON")), r['lat'], r['lon'])
+    float(env("LAT")), float(env("LON")), r["lat"], r["lon"]
+)
 
-if distance_from_house <= 1 and int(percentage) < 60 and battery_status == 'discharging':
-    embed = DiscordEmbed(title='Mac Battery Low')
+if (
+    distance_from_house <= 1
+    and int(percentage) < 60
+    and battery_status == "discharging"
+) or (int(percentage) <= 30 and battery_status == "discharging"):
+    embed = DiscordEmbed(title="Mac Battery Low")
 
-    embed.add_embed_field(
-        name="Data", value=f"Percentage:\t{percentage}", inline=False)
+    embed.add_embed_field(name="Data", value=f"Percentage:\t{percentage}", inline=False)
 
     # set footer
-    embed.set_footer(text='Made By Ibrahim Mudassar',
-                     icon_url='https://avatars.githubusercontent.com/u/22484328?v=4')
+    embed.set_footer(
+        text="Made By Ibrahim Mudassar",
+        icon_url="https://avatars.githubusercontent.com/u/22484328?v=4",
+    )
 
     # add embed object to webhook(s)
     # Webhooks to send to
